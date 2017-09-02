272190f8e45d6af87e4c430493063ababc77d2aa
Unknown <158840284@qq.com>
2017-09-02 01:53:56 -0500

    fatching data from mysql

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Post;
 
 class PostsController extends Controller
 {
@@ -13,7 +14,8 @@ class PostsController extends Controller
      */
     public function index()
     {
-        //
+			$posts = Post::orderBy('created_at','desc')->paginate(10);
+      return view('posts.index')->with('posts', $posts);
     }
 
     /**
@@ -45,7 +47,8 @@ class PostsController extends Controller
      */
     public function show($id)
     {
-        //
+			$posts = Post::find($id);
+			return view('posts.show')->with('post', $posts);
     }
 
     /**
